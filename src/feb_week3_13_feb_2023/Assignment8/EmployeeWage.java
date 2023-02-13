2ce98dd7992eff37c2023e12f264df990d71c498
sujan-coditas <sujan.shaikh@coditas.com>
2023-02-13 23:02:41 +0530

    Assignment 8

--- src/feb_week3_13_feb_2023/Assignment8/EmployeeWage.java
+++ src/feb_week3_13_feb_2023/Assignment8/EmployeeWage.java
@@ -5,7 +5,6 @@ class Employee {
     private String name;
     private int id;
     public Employee() {}
-
     public Employee(String name, int id) {
         this.name = name;
         this.id = id;
