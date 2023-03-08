673a351f40e3642126c77c631e3c513b26b2a865
sujan-coditas <sujan.shaikh@coditas.com>
2023-03-08 14:13:44 +0530

    Assignment updated

--- src/March_Assignments_2023/week2/Assignment17/Q5_HashsetToArray.java
+++ src/March_Assignments_2023/week2/Assignment17/Q5_HashsetToArray.java
@@ -2,6 +2,7 @@ package March_Assignments_2023.week2.Assignment17;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class Q5_HashsetToArray {
     public static void main(String[] args) {
@@ -11,10 +12,20 @@ public class Q5_HashsetToArray {
         name.add("shaikh");
         System.out.println("Printing Hashset values ::" + name);
 
-        String fullname[]=new String[name.size()];
-        fullname=name.toArray(fullname);
+//        String fullname[]=new String[name.size()];
+//        fullname=name.toArray(fullname);
+//
+//        System.out.println("Displaying Hashset to Array : " + Arrays.toString(fullname));
 
-        System.out.println("Displaying Hashset to Array : " + Arrays.toString(fullname));
+        Iterator<String> iterator = name.iterator();
+        String first = iterator.next();
+        System.out.println("First element: " + first);
+
+        String last = null;
+        while (iterator.hasNext()) {
+            last = iterator.next();
+        }
+        System.out.println("Last element: " + last);
 
 
 
